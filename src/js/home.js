001353865e19390d5a67828ba13de60dc32f18d1
IMTR0J4NV2 <jjorgebeau@edenschool.fr>
2022-10-21 13:17:51 +0200

    Edit main menu

--- src/js/home.js
+++ src/js/home.js
@@ -7,10 +7,12 @@ export function renderMainMenu() {
 function displayButtons() {
     let startButton = document.createElement('input');
     let customButton = document.createElement('input');
+    let startText = document.createElement('p');
 
     startButton.type = 'button';
     customButton.type = 'button';
 
+    startButton.value = "Jouer"
     document.body.appendChild(startButton);
     document.body.appendChild(customButton);
 }
